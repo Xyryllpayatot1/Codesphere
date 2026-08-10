0823ae9743d6c011c27176385d4aad3f855b9764
Xyryllpayatot1 <jhonxyryll@gmail.com>
2026-08-10 14:21:37 +0900

    Serve atom.xml with application/xml content type for Google

--- src/app/atom.xml/route.ts
+++ src/app/atom.xml/route.ts
@@ -75,7 +75,7 @@ export async function GET() {
 
   return new Response(xml, {
     headers: {
-      "Content-Type": "application/atom+xml; charset=utf-8",
+      "Content-Type": "application/xml; charset=utf-8",
       "Cache-Control": "public, max-age=3600",
     },
   });
